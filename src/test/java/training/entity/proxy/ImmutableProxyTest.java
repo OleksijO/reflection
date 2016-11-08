f85b6371e16a272729eb619e3cf786c1b5691d70
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-11-08 13:40:21 +0200

    Task 5 implemented. Immutable Proxy Test added

---
+++ src/test/java/training/entity/proxy/ImmutableProxyTest.java
@@ -0,0 +1,49 @@
+package training.entity.proxy;
+
+import org.junit.Test;
+import training.proxy.ImmutableProxy;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by oleksij.onysymchuk@gmail on 08.11.2016.
+ */
+public class ImmutableProxyTest {
+
+    @Test (expected = UnsupportedOperationException.class)
+    public void getImmutableProxyTest() {
+        TestInterfaceImpl testObj = new TestInterfaceImpl();
+        assertEquals(-1, testObj.getField());
+        testObj.setField(1);
+        assertEquals(1, testObj.getField());
+        TestInterface immutable = ImmutableProxy.getImmutableProxy(testObj);
+        assertTrue(immutable instanceof TestInterface);
+        assertEquals(1, immutable.getField());
+        testObj.setField(2);
+        assertEquals(2, immutable.getField());
+        immutable.setField(-1);
+    }
+
+
+    public interface TestInterface {
+        int getField();
+        void setField(int i);
+    }
+
+    public class TestInterfaceImpl implements TestInterface {
+        private int field = -1;
+
+        @Override
+        public int getField() {
+            return field;
+        }
+
+        @Override
+        public void setField(int field) {
+            this.field = field;
+        }
+    }
+
+
+}
